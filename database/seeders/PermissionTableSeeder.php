371446440111e6ccda80331e55495c5425c3d861
Ahmed Elmaghraby <ahmed.elmaghraby207@gmail.com>
2024-05-28 14:33:08 +0300

    enhancing the permissions seeder

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -1,11 +1,12 @@
 <?php
-  
+
 namespace Database\Seeders;
-  
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
-  
+
 class PermissionTableSeeder extends Seeder
 {
     /**
@@ -14,18 +15,20 @@ class PermissionTableSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
-           'role-list',
-           'role-create',
-           'role-edit',
-           'role-delete',
-           'product-list',
-           'product-create',
-           'product-edit',
-           'product-delete'
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete'
         ];
-        
-        foreach ($permissions as $permission) {
-             Permission::create(['name' => $permission]);
+
+        if (DB::table('permissions')->count() == 0) {
+            foreach ($permissions as $permission) {
+                Permission::create(['name' => $permission]);
+            }
         }
     }
 }
