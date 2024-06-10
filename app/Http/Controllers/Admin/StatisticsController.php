94e11e51d6dbdf93325b851764b6766f8521a051
gerges <gerges.hana@x-era.ae>
2024-06-10 17:55:40 +0300

    ecommerce ChartView

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -33,6 +33,88 @@ class StatisticsController extends Controller
     $ordersTodayStatus5 = PurchaseOrder::query()->whereDate('arrival_date', $today)->where('status_id',5)->get()->count();
     $ordersTodayStatus6 = PurchaseOrder::query()->whereDate('arrival_date', $today)->where('status_id',6)->get()->count();
 
+
+
+    $endOfWeek = $today->copy()->addDays(0);   
+    $startOfWeek = $today->copy()->subDays(7);  
+    $dailyOrders = [];
+    $dailyTotals = [];
+    for ($i = 0; $i < 8; $i++) {
+        $date = $endOfWeek->copy()->subDays($i)->toDateString();
+        $dailyOrders[$date] = PurchaseOrder::whereDate('created_at', $date)->count();
+    }
+    foreach ($dailyOrders as $date => $count) {
+        $dailyTotals[] = $count;
+    }
+
+    // Define daily variables
+    $day_1 = $dailyTotals[0] ?? 0;
+    $day_2 = $dailyTotals[1] ?? 0;
+    $day_3 = $dailyTotals[2] ?? 0;
+    $day_4 = $dailyTotals[3] ?? 0;
+    $day_5 = $dailyTotals[4] ?? 0;
+    $day_6 = $dailyTotals[5] ?? 0;
+    $day_7 = $dailyTotals[6] ?? 0;
+    $day_8 = $dailyTotals[7] ?? 0;
+
+
+
+
+
+
+
+
+    $weeklyOrders = [];
+
+    for ($i = 0; $i < 8; $i++) {
+        $startOfWeek = $today->copy()->subWeeks($i)->startOfWeek()->toDateString();
+        $endOfWeek = $today->copy()->subWeeks($i)->endOfWeek()->toDateString();
+        $weeklyOrders[$i] = PurchaseOrder::whereBetween('created_at', [$startOfWeek, $endOfWeek])->count();
+    }
+
+    // Define weekly variables
+    $week_1 = $weeklyOrders[0] ?? 0;
+    $week_2 = $weeklyOrders[1] ?? 0;
+    $week_3 = $weeklyOrders[2] ?? 0;
+    $week_4 = $weeklyOrders[3] ?? 0;
+    $week_5 = $weeklyOrders[4] ?? 0;
+    $week_6 = $weeklyOrders[5] ?? 0;
+    $week_7 = $weeklyOrders[6] ?? 0;
+    $week_8 = $weeklyOrders[7] ?? 0;
+
+
+
+
+
+
+
+
+
+
+    $monthlyOrders = [];
+
+    for ($i = 0; $i < 12; $i++) {
+        $startOfMonth = $today->copy()->subMonths($i)->startOfMonth()->toDateString();
+        $endOfMonth = $today->copy()->subMonths($i)->endOfMonth()->toDateString();
+        $monthlyOrders[$i] = PurchaseOrder::whereBetween('created_at', [$startOfMonth, $endOfMonth])->count();
+    }
+
+    // Define monthly variables
+    $month_1 = $monthlyOrders[0] ?? 0;
+    $month_2 = $monthlyOrders[1] ?? 0;
+    $month_3 = $monthlyOrders[2] ?? 0;
+    $month_4 = $monthlyOrders[3] ?? 0;
+    $month_5 = $monthlyOrders[4] ?? 0;
+    $month_6 = $monthlyOrders[5] ?? 0;
+    $month_7 = $monthlyOrders[6] ?? 0;
+    $month_8 = $monthlyOrders[7] ?? 0;
+    $month_9 = $monthlyOrders[8] ?? 0;
+    $month_10 = $monthlyOrders[9] ?? 0;
+    $month_11 = $monthlyOrders[10] ?? 0;
+    $month_12 = $monthlyOrders[11] ?? 0;
+
+
+
     return view('admin.home.index', compact(
         'allPurchaseOrder',
         'countStatus1', 
@@ -50,6 +132,12 @@ class StatisticsController extends Controller
         'ordersTodayStatus4',
         'ordersTodayStatus5',
         'ordersTodayStatus6',
+        'dailyOrders',
+        'dailyTotals',
+        'day_1', 'day_2', 'day_3', 'day_4', 'day_5', 'day_6', 'day_7', 'day_8',
+        'week_1', 'week_2', 'week_3', 'week_4', 'week_5', 'week_6', 'week_7', 'week_8',
+        'month_1', 'month_2', 'month_3', 'month_4', 'month_5', 'month_6', 'month_7', 'month_8', 'month_9', 'month_10', 'month_11', 'month_12'
+
     
     ));
     }
--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -109,7 +109,7 @@
 </section>
 
 
-    <h2 style="font-size: 20px;">طلبيات اليوم</h2>
+<h2 style="font-size: 20px;">طلبيات اليوم</h2>
 <section id="minimal-statistics">
     <div class="row">
         <div class="col">
@@ -226,8 +226,6 @@
                 </div>
             </div>
         </div>
-
-
         <!-- Simple Doughnut Chart -->
         <div class="col-md-6 col-sm-12">
             <div class="card">
@@ -251,34 +249,23 @@
         </div>
     </div>
 
-
-
-
-
-
-
-
-
-
-
-
     <!-- Products sell and New Orders -->
     <div class="row match-height">
         <div class="col-xl-12 col-12" id="ecommerceChartView">
             <div class="card card-shadow">
                 <div class="card-header card-header-transparent py-20">
                     <div class="btn-group dropdown">
-                        <a href="#" class="text-body dropdown-toggle blue-grey-700" data-toggle="dropdown">PRODUCTS SALES</a>
-                        <div class="dropdown-menu animate" role="menu">
+                        <a href="#" class="text-body blue-grey-700">احصائيه الطلبيات المطلوبه </a>
+                        <!-- <div class="dropdown-menu animate" role="menu">
                             <a class="dropdown-item" href="#" role="menuitem">Sales</a>
                             <a class="dropdown-item" href="#" role="menuitem">Total sales</a>
                             <a class="dropdown-item" href="#" role="menuitem">profit</a>
-                        </div>
+                        </div> -->
                     </div>
                     <ul class="nav nav-pills nav-pills-rounded chart-action float-right btn-group" role="group">
-                        <li class="nav-item"><a class="active nav-link" data-toggle="tab" href="#scoreLineToDay">Day</a></li>
-                        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#scoreLineToWeek">Week</a></li>
-                        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#scoreLineToMonth">Month</a></li>
+                        <li class="nav-item"><a class="active nav-link" data-toggle="tab" href="#scoreLineToDay">اليوم</a></li>
+                        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#scoreLineToWeek">الاسبوع</a></li>
+                        <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#scoreLineToMonth">الشهر</a></li>
                     </ul>
                 </div>
                 <div class="widget-content tab-content bg-white p-20">
@@ -291,14 +278,16 @@
     </div>
     <!--/ Products sell and New Orders -->
 
-
-
-
-
-
 </section>
 <!-- End Pie charts section -->
 @endsection
+
+
+
+
+
+
+
 
 @section('page-script-files')
 
@@ -311,85 +300,387 @@
 <script src="{{asset('/app-assets/vendors/js/charts/jvector/jquery-jvectormap-2.0.3.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('/app-assets/vendors/js/charts/jvector/jquery-jvectormap-world-mill.js')}}" type="text/javascript"></script>
 <script src="{{asset('/app-assets/data/jvector/visitor-data.js')}}" type="text/javascript"></script>
-<script src="{{asset('/app-assets/js/scripts/charts/chartjs/pie-doughnut/pie.js')}}" type="text/javascript"></script>
-
 
 
 <!-- <script src="{{asset('/app-assets/js/scripts/charts/chartjs/pie-doughnut/pie-simple.js')}}" type="text/javascript"></script> -->
-
-
-
-
-<script src="{{asset('/app-assets/js/scripts/charts/chartjs/pie-doughnut/doughnut.js')}}" type="text/javascript"></script>
 <!-- <script src="{{asset('/app-assets/js/scripts/charts/chartjs/pie-doughnut/doughnut-simple.js')}}" type="text/javascript"></script> -->
 @endsection
 
 @section('scripts')
 
+<!-- ==========================staret ecommerce ChartView========================================= -->
+<script>
+    $(window).on("load", function() {
+        $('#recent-buyers, #new-orders').perfectScrollbar({
+            wheelPropagation: true
+        });
+
+        /********************************************
+         *               Monthly Sales               *
+         ********************************************/
+        Morris.Bar.prototype.fillForSeries = function(i) {
+            var color;
+            return "0-#fff-#f00:20-#000";
+        };
+
+        Morris.Bar({
+            element: 'monthly-sales',
+            data: [{
+                month: 'Jan',
+                sales: 1835
+            }, {
+                month: 'Feb',
+                sales: 2356
+            }, {
+                month: 'Mar',
+                sales: 1459
+            }, {
+                month: 'Apr',
+                sales: 1289
+            }, {
+                month: 'May',
+                sales: 1647
+            }, {
+                month: 'Jun',
+                sales: 2156
+            }, {
+                month: 'Jul',
+                sales: 1835
+            }, {
+                month: 'Aug',
+                sales: 2356
+            }, {
+                month: 'Sep',
+                sales: 1459
+            }, {
+                month: 'Oct',
+                sales: 1289
+            }, {
+                month: 'Nov',
+                sales: 1647
+            }, {
+                month: 'Dec',
+                sales: 2156
+            }],
+            xkey: 'month',
+            ykeys: ['sales'],
+            labels: ['Sales'],
+            barGap: 4,
+            barSizeRatio: 0.3,
+            gridTextColor: '#bfbfbf',
+            gridLineColor: '#E4E7ED',
+            numLines: 5,
+            gridtextSize: 14,
+            resize: true,
+            barColors: ['#FF394F'],
+            hideHover: 'auto',
+        });
+
+    });
+
+
+
+
+
+    (function(window, document, $) {
+        'use strict';
+        /*************************************************
+         *               Score Chart                      *
+         *************************************************/
+        (function() {
+            var scoreChart = function scoreChart(id, labelList, series1List) {
+                var scoreChart = new Chartist.Line('#' + id, {
+                    labels: labelList,
+                    series: [series1List]
+                }, {
+                    lineSmooth: Chartist.Interpolation.simple({
+                        divisor: 2
+                    }),
+                    fullWidth: true,
+                    chartPadding: {
+                        right: 25
+                    },
+                    series: {
+                        "series-1": {
+                            showArea: false
+                        }
+                    },
+                    axisX: {
+                        showGrid: false
+                    },
+                    axisY: {
+                        labelInterpolationFnc: function labelInterpolationFnc(value) {
+                            return value / 1000 + 'K';
+                        },
+                        scaleMinSpace: 40
+                    },
+                    plugins: [Chartist.plugins.tooltip()],
+                    low: 0,
+                    showPoint: false,
+                    height: 300
+                });
+
+                scoreChart.on('created', function(data) {
+                    var defs = data.svg.querySelector('defs') || data.svg.elem('defs');
+                    var width = data.svg.width();
+                    var height = data.svg.height();
+
+                    var filter = defs.elem('filter', {
+                        x: 0,
+                        y: "-10%",
+                        id: 'shadow' + id
+                    }, '', true);
+
+                    filter.elem('feGaussianBlur', {
+                        in: "SourceAlpha",
+                        stdDeviation: "24",
+                        result: 'offsetBlur'
+                    });
+                    filter.elem('feOffset', {
+                        dx: "0",
+                        dy: "32"
+                    });
+
+                    filter.elem('feBlend', {
+                        in: "SourceGraphic",
+                        mode: "multiply"
+                    });
+
+                    defs.elem('linearGradient', {
+                        id: id + '-gradient',
+                        x1: 0,
+                        y1: 0,
+                        x2: 1,
+                        y2: 0
+                    }).elem('stop', {
+                        offset: 0,
+                        'stop-color': 'rgba(22, 141, 238, 1)'
+                    }).parent().elem('stop', {
+                        offset: 1,
+                        'stop-color': 'rgba(98, 188, 246, 1)'
+                    });
+
+                    return defs;
+                }).on('draw', function(data) {
+                    if (data.type === 'line') {
+                        data.element.attr({
+                            filter: 'url(#shadow' + id + ')'
+                        });
+                    } else if (data.type === 'point') {
+
+                        var parent = new Chartist.Svg(data.element._node.parentNode);
+                        parent.elem('line', {
+                            x1: data.x,
+                            y1: data.y,
+                            x2: data.x + 0.01,
+                            y2: data.y,
+                            "class": 'ct-point-content'
+                        });
+                    }
+                    if (data.type === 'line' || data.type == 'area') {
+                        data.element.animate({
+                            d: {
+                                begin: 1000 * data.index,
+                                dur: 1000,
+                                from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+                                to: data.path.clone().stringify(),
+                                easing: Chartist.Svg.Easing.easeOutQuint
+                            }
+                        });
+                    }
+                });
+            };
+
+            var DayLabelList = ["اليوم", "الامس", "من 3 ايام", "من 4 ايام", "من 5 ايام", "من 6 ايام", "من 7 ايام", "من 8 ايام"];
+
+var DaySeries1List = {
+    name: "series-1",
+    data: [{{$day_1}}, {{$day_2}}, {{$day_3}}, {{$day_4}}, {{$day_5}}, {{$day_6}}, {{$day_7}}, {{$day_8}}]
+};
+
+var WeekLabelList = ["الاسبوع الاول ", "الاسبوع الثاني", "الاسبوع الثالث", "الاسبوع الرابع ", "الاسبوع الخامس", "الاسبوع السادس", "الاسبوع السابع", "الاسبوع الثامن"];
+
+var WeekSeries1List = {
+    name: "series-1",
+    data: [{{$week_1}},{{$week_2}},{{$week_3}},{{$week_4}},{{$week_5}},{{$week_6}},{{$week_7}},{{$week_8}}]
+};
+
+var MonthLabelList = ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
+
+var MonthSeries1List = {
+    name: "series-1",
+    data: [{{$month_1}}, {{$month_2}}, {{$month_3}}, {{$month_4}}, {{$month_5}}, {{$month_6}}, {{$month_7}}, {{$month_8}}, {{$month_9}}, {{$month_10}}, {{$month_11}}, {{$month_12}}]
+};
+
+
+            var createChart = function createChart(button) {
+                var btn = button || $("#ecommerceChartView .chart-action").find(".active");
+
+                var chartId = btn.attr("href");
+                switch (chartId) {
+                    case "#scoreLineToDay":
+                        scoreChart("scoreLineToDay", DayLabelList, DaySeries1List);
+                        break;
+                    case "#scoreLineToWeek":
+                        scoreChart("scoreLineToWeek", WeekLabelList, WeekSeries1List);
+                        break;
+                    case "#scoreLineToMonth":
+                        scoreChart("scoreLineToMonth", MonthLabelList, MonthSeries1List);
+                        break;
+                }
+            };
+
+            createChart();
+            $(".chart-action li a").on("click", function() {
+                createChart($(this));
+            });
+        })();
+
+        /*************************************************
+         *               Cost Revenue Stats               *
+         *************************************************/
+        new Chartist.Line('#cost-revenue', {
+            labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            series: [
+                [{
+                        meta: 'Revenue',
+                        value: 5
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 3
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 6
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 3
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 8
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 5
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 8
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 12
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 7
+                    },
+                    {
+                        meta: 'Revenue',
+                        value: 14
+                    },
+
+                ]
+            ]
+        }, {
+            low: 0,
+            high: 18,
+            fullWidth: true,
+            showArea: true,
+            showPoint: true,
+            showLabel: false,
+            axisX: {
+                showGrid: false,
+                showLabel: false,
+                offset: 0
+            },
+            axisY: {
+                showGrid: false,
+                showLabel: false,
+                offset: 0
+            },
+            chartPadding: 0,
+            plugins: [
+                Chartist.plugins.tooltip()
+            ]
+        }).on('draw', function(data) {
+            if (data.type === 'area') {
+                data.element.attr({
+                    'style': 'fill: #28D094; fill-opacity: 0.2'
+                });
+            }
+            if (data.type === 'line') {
+                data.element.attr({
+                    'style': 'fill: transparent; stroke: #28D094; stroke-width: 4px;'
+                });
+            }
+            if (data.type === 'point') {
+                var circle = new Chartist.Svg('circle', {
+                    cx: [data.x],
+                    cy: [data.y],
+                    r: [7],
+                }, 'ct-area-circle');
+                data.element.replace(circle);
+            }
+        });
+    })(window, document, jQuery);
+</script>
+<!-- ==========================end ecommerce ChartView============================================= -->
 
 <!-- ==========================stare simple-pie-chart===================================== -->
 <script>
-    $(window).on("load", function() {
-        var ctx = $("#simple-pie-chart");
-        var chartOptions = {
-            responsive: true,
-            maintainAspectRatio: false,
-            responsiveAnimationDuration: 500,
-        };
-        var chartData = {
-            labels: ["تم الارسال", "تم الدخول", "تم الوصول", "تم الالغاء", "تم المغادره"],
-            datasets: [{
-                label: "حاله الطلبيات",
-                data: [{{$countStatus1}}, {{$countStatus4}}, {{$countStatus3}}, {{$countStatus2}} , {{$countStatus6}}],
-                backgroundColor: ['#00A5A8', '#626E82', '#FF7D4D', '#FF4558', '#28D094'],
-            }]
-        };
-        var config = {
-            type: 'pie',
-            options: chartOptions,
-
-            data: chartData
-        };
-        var pieSimpleChart = new Chart(ctx, config);
-    });
-</script>
+        $(window).on("load", function() {
+            var ctx = $("#simple-pie-chart");
+            var chartOptions = {
+                responsive: true,
+                maintainAspectRatio: false,
+                responsiveAnimationDuration: 500,
+            };
+            var chartData = {
+                labels: ["تم الارسال", "تم الدخول", "تم الوصول", "تم الالغاء", "تم المغادره"],
+                datasets: [{
+                    label: "حاله الطلبيات",
+                    data: [{{ $countStatus1 }}, {{ $countStatus2 }}, {{ $countStatus3 }}, {{ $countStatus4 }}, {{ $countStatus5 }}],
+                    backgroundColor: ['#00A5A8', '#626E82', '#FF7D4D', '#FF4558', '#28D094'],
+                }]
+            };
+            var config = {
+                type: 'pie',
+                options: chartOptions,
+                data: chartData
+            };
+            var pieSimpleChart = new Chart(ctx, config);
+        });
+    </script>
 <!-- ==========================end simple-pie-chart===================================== -->
-
-
-
-
-<!-- ==========================start simple-doughnut-chart===================================== -->
 <script>
+        $(window).on("load", function() {
+            var ctx = $("#simple-doughnut-chart");
+            var chartOptions = {
+                responsive: true,
+                maintainAspectRatio: false,
+                responsiveAnimationDuration: 500,
+            };
+            var chartData = {
+                labels: ["الطلبيات القادمه اليوم", "الطلبيات الملغاه اليوم", "الطلبيات الواصله", "الطلبات المدخله اليوم", "الطلبات المكتمله اليوم"],
+                datasets: [{
+                    label: "طلبيه اليوم",
+                    data: [{{ $ordersTodayStatus1 }}, {{ $ordersTodayStatus2 }}, {{ $ordersTodayStatus3 }}, {{ $ordersTodayStatus4 }}, {{ $ordersTodayStatus6 }}],
+                    backgroundColor: ['#00A5A8', '#626E82', '#FF7D4D', '#FF4558', '#28D094'],
+                }]
+            };
+            var config = {
+                type: 'doughnut',
+                options: chartOptions,
+                data: chartData
+            };
 
-$(window).on("load", function(){
-var ctx = $("#simple-doughnut-chart");
-var chartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    responsiveAnimationDuration:500,
-};
-var chartData = {
-    labels: ["الطلبيات القادمه اليوم", "الطلبيات الملغاه اليوم", "الطلبيات الواصله", "الطلبات المدخله اليوم", "الطلبات المكتمله اليوم"],
-    datasets: [{
-        label: "طلبيه اليوم",
-        data: [{{$ordersTodayStatus1}}, {{$ordersTodayStatus2}}, {{$ordersTodayStatus3}}, {{$ordersTodayStatus4}}, {{$ordersTodayStatus6}}],
-        backgroundColor: ['#00A5A8', '#626E82', '#FF7D4D','#FF4558', '#28D094'],
-    }]
-};
-var config = {
-    type: 'doughnut',
-    options : chartOptions,
-    data : chartData
-};
-
-var doughnutSimpleChart = new Chart(ctx, config);
-});
-</script>
+            var doughnutSimpleChart = new Chart(ctx, config);
+        });
+    </script>
 <!-- ==========================end simple-doughnut-chart===================================== -->
 
-
-@endsection
-@section('scripts')
-<!-- <script src="{{asset('/app-assets/js/scripts/pages/dashboard-sales.js')}}" type="text/javascript"></script> -->
-<script src="{{asset('/app-assets/js/scripts/pages/dashboard-ecommerce.js')}}" type="text/javascript"></script>
 @endsection
